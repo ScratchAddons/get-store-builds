a9b4028eb40c38229c957b98c9a02c0e1a0b1a96
Hans5958 <email5958.1@gmail.com>
2020-11-08 10:29:24 +0000

    Fix downloading from Chrome Web Store

--- script.sh
+++ script.sh
@@ -3,8 +3,11 @@ VERSION_TAG=$(curl \
   https://api.github.com/repos/ScratchAddons/ScratchAddons/releases \
   | jq -r '.[0].tag_name')
 
-wget -O scratchaddons-$VERSION_TAG.crx \
-  https://clients2.google.com/service/update2/crx?response=redirect&prodversion=86.0.4240.111&acceptformat=crx2,crx3&x=id%3Dfbeffbjdlemaoicjdapfpikkikjoneco%26uc
+wget \
+  -O scratchaddons-$VERSION_TAG.crx \
+  -U "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4230.1 Safari/537.36" \
+  "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=86.0.4230.1&acceptformat=crx2,crx3&x=id%3Dfbeffbjdlemaoicjdapfpikkikjoneco%26uc"
 
-wget -O scratchaddons-$VERSION_TAG.xpi \
-  http://addons.mozilla.org/firefox/downloads/latest/scratch-messaging-extension/addon-12999334-latest.xpi
+wget \
+  -O scratchaddons-$VERSION_TAG.xpi \
+  https://addons.mozilla.org/firefox/downloads/latest/scratch-messaging-extension/addon-12999334-latest.xpi
